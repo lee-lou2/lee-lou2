f19da7f96e71265afcefd40a375b064a81a81d33
jake <jake@millie.town>
2023-06-02 14:02:08 +0900

    rm, ChatGPT API 테스트 코드 제거

--- contents/docs/project/index/chat/src/chat.js
+++ contents/docs/project/index/chat/src/chat.js
@@ -9,7 +9,7 @@ function requestChatGPTAPI(apiServerHost) {
   const questionInput = document.getElementById("chatgpt-request-body-api");
   const answerOutput = document.getElementById("chatgpt-response-body-api");
   answerOutput.innerHTML = '작성중...';
-  const url = apiServerHost + "/v1/ai/chat";
+  const url = apiServerHost + "/v1/ai/wrtn";
   fetch(url, {
     method: 'POST',
     headers: {
@@ -23,40 +23,11 @@ function requestChatGPTAPI(apiServerHost) {
   });
 }
 
-function requestChatGPTSlack(apiServerHost) {
-  const questionInput = document.getElementById("chatgpt-request-body-slack");
-  const answerOutput = document.getElementById("chatgpt-response-body-slack");
-  const url = apiServerHost + "/v1/ai/dataset";
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "instruction": questionInput.value,
-      "category": "slack"
-    })
-  }).then(response => {
-    if (response.status == 200) {
-      answerOutput.value = '슬랙으로 답변 전송';
-    } else {
-      answerOutput.value = '답변 생성간 오류 발생';
-    }
-  });
-}
-
 export default function ChatGPTTest() {
   const { siteConfig } = useDocusaurusContext();
   const apiServerHost = siteConfig.customFields.API_SERVER_HOST;
   return (
     <div>
-      <h2>ChatGPT Socket 테스트(슬랙으로 답변 전송)</h2>
-      <div className={styles.chatTestElements}>
-        <TextField id="chatgpt-request-body-slack" label="질문" size="small" placeholder="질문을 입력해주세요" />
-        <Button variant="contained" onClick={() => { requestChatGPTSlack(apiServerHost) }}>입력</Button>
-        <TextField id="chatgpt-response-body-slack" value="상태" size="small" disabled />
-      </div>
-      <br />
       <h2>ChatGPT API 테스트</h2>
       <div className={styles.chatTestElements}>
         <TextField id="chatgpt-request-body-api" label="질문" size="small" placeholder="질문을 입력해주세요" />
